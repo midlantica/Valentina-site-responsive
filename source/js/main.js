38630e35752cc64b4d0738626bcb9cdebee2c514
Andrew Harper <druharper@gmail.com>
2015-06-18 11:59:01 -0500

    jquery experiment

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,19 @@
 
+// $('.mainNav ul li a').on('click', function(e) {
+//     e.preventDefault();
+//     var url = this.href;
+
+//     // $('nav a.current').removeClass('current');
+//     // $(this).addClass('current');
+
+//     $('.content').hide();
+//     $('.content').load(url + ' .content');
+//     $('.content').show();
+
+// });
+
+
+// .fadeOut(100)
+// .fadeIn(400)
+
+// $('.gallery ul li').load
