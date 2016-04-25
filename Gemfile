d112486db65a9c30119387955a768ef9241c2a50
Andrew Harper <druharper@gmail.com>
2016-04-25 10:32:44 -0500

    Post clean MM install

--- Gemfile
+++ Gemfile
@@ -1,15 +1,12 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-gem 'middleman', '~>4.1.7'
-# gem 'middleman', '~>3.3.10'
+#######################
 
-gem "middleman-compass"
-# gem 'middleman-compass', '>= 4.0.0'
+gem 'sass'
+# gem 'susy'
+gem 'bourbon'
+gem 'neat'
 
-# gem "middleman-sprockets", "~> 3.4.2"
-gem "middleman-sprockets", "~> 4.0.0.rc.2"
-
-gem 'middleman-livereload', '~> 3.4.3'
 
 # For faster file watcher updates on Windows:
 gem 'wdm', '~> 0.1.0', :platforms => [:mswin, :mingw]
@@ -20,15 +17,23 @@ gem 'tzinfo-data', platforms: [:mswin, :mingw]
 # gem 'therubyracer', '~> 3.16.14.13' # faster JS compiles
 gem 'oj' # faster JS compiles
 
-#######################
+gem 'middleman', '>= 4.0.0'
+# gem 'middleman', '~>4.1.7'
+# gem 'middleman', '~>3.3.10'
 
-gem 'sass'
-# gem 'susy'
-gem 'bourbon'
-gem 'neat'
+gem "middleman-compass"
+# gem 'middleman-compass', '>= 4.0.0'
+
+# gem "middleman-sprockets", "~> 3.4.2"
+# gem "middleman-sprockets", "~> 4.0.0.rc.2"
+
+gem 'middleman-livereload'
+# gem 'middleman-livereload', '~> 3.4.3'
 
 ########
 # gem 'middleman-bower'
+# DOESN'T WORK WITH MIDDLEMAN 4
+
 gem 'middleman-minify-html'
 
 # https://github.com/middleman-contrib/middleman-deploy
@@ -37,5 +42,9 @@ gem 'middleman-minify-html'
 
 gem 'middleman-autoprefixer'
 
+# gem 'image_optim'
+# gem 'image_optim', :git => 'git://github.com/toy/image_optim.git'
+# gem 'image_optim_pack'
 # https://github.com/plasticine/middleman-imageoptim
 # gem 'middleman-imageoptim'
+
--- config.rb
+++ config.rb
@@ -1,78 +1,42 @@
 ###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-# activate :compass
-
-set :sass_dir,   'css'
-set :scss_dir,   'css'
-set :css_dir,    'css'
-set :js_dir,     'js'
-set :images_dir, 'img'
-set :fonts_dir,  'css/fonts'
-
-#----------------------------
-
-#Folder settings
-# http_path = ""
-# project_type    = :stand_alone
-# relative_assets = true        # because we're not working from the root
-# sass_dir        = "css"       # where our .scss files are
-# css_dir         = "css"       # where the CSS will saved
-# images_dir      = "img"       # the folder with your images
-# fonts_dir       = "scss/fonts" # the folder with your fonts
-
-
-###
 # Page options, layouts, aliases and proxies
 ###
 
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
 
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+# Automatic image dimensions on image_tag helper
+set :sass_dir,   'css'
+set :scss_dir,   'css'
+set :css_dir,    'css'
+set :js_dir,     'js'
+set :images_dir, 'img'
+set :fonts_dir,  'css/fonts'
+# set :build_dir,  'build'
+# set :build_dir, ENV['MM_BUILD_DIR']
+
+activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
 
 ###
 # Helpers
 ###
-
-# Automatic image dimensions on image_tag helper
-activate :automatic_image_sizes
-
-# activate :bower
-# import_path File.expand_path('bower_components', app.root)
-# sprockets.append_path File.join "#{root}", "bower_components"
-
-# https://github.com/middleman/middleman-autoprefixer
-activate :autoprefixer do |config|
-  config.browsers = ['last 2 versions', '>5%']
-  config.cascade  = true
-  config.inline   = true
-  # config.ignore   = ['hacks.css']
-end
-
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload, port: '4567', livereload_css_target: "css/style.css"
-  # activate :minify_css
-
-end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -81,25 +45,16 @@ end
 #   end
 # end
 
-
 # Build-specific configuration
 configure :build do
-
   activate :relative_assets
-
   activate :minify_css
   activate :minify_html
   activate :minify_javascript
-  activate :imageoptim
-
+  # activate :imageoptim
   activate :gzip
-
-  # activate :directory_indexes
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
-
 end
+
 
 # https://github.com/middleman-contrib/middleman-deploy
 # activate :deploy do |deploy|
@@ -119,6 +74,3 @@ end
 #   deploy.user     = 'tvaughan'
 #   deploy.password = 'secret'
 # end
-
-
-
